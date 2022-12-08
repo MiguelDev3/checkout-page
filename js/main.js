5d4858f26ddb353736a81117b977412abc0a6c98
MiguelDev3 <miguel.edu.chacon.3@gmail.com>
2022-12-08 03:26:18 -0500

    Actualizar validacion del Formulario

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@
     const $alertBack = d.querySelector('.alert-bg');
     const $alertForm = d.querySelector('.form-alert');
     const $buttonAlert = d.querySelector('#btn-reload');
+    const $checkForm = d.querySelector('#main-form')
 
     $listProducts.addEventListener('click', function(e){
         $plusButtons.forEach(button => {
@@ -23,8 +24,19 @@
     })
     $buttonContinue.addEventListener('click', function(e){
         e.preventDefault();
-        $alertBack.classList.remove('hide');
-        $alertForm.classList.remove('hide');
+        let $formElements = $checkForm.querySelectorAll(':invalid');
+        if($formElements.length === 0){
+            console.log("UFF PAPA!")
+            $alertBack.classList.remove('hide');
+            $alertForm.classList.remove('hide');
+        }else{
+            $formElements.forEach(el => {
+                el.parentElement.classList.add('shake');
+                el.parentElement.classList.add('invalid');
+                setTimeout(() => el.parentElement.classList.remove('shake'), 1000);
+            })
+        }
+        
     })
     $buttonAlert.addEventListener('click', function(){
         $alertBack.classList.add('hide');
